0179ce26b0e29af5f30a3bd300837781def274e1
fabiobatalha <fabiobatalha@gmail.com>
2017-06-12 11:24:53 -0300

    Ajustes para definir range de período de custos

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ tests_require = [
 
 setup(
     name="doi_request",
-    version="1.1.0",
+    version="1.1.1",
     description="Tool to manage the DOI registering process",
     long_description=README + '\n\n' + CHANGES,
     author="SciELO",
