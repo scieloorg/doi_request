e579ba313c307f6563aa336eb0cba79d6004ce9a
Gustavo Fonseca <gustavo@gfonseca.net>
2018-07-23 16:26:46 -0300

    Remove trecho de código morto

--- doi_request/controller.py
+++ doi_request/controller.py
@@ -1,20 +1,7 @@
-import pickle
-
-from tasks.celery import (
-    registry_dispatcher_document,
-    registry_dispatcher
-)
+from tasks.celery import registry_dispatcher
 
 
 class Depositor(object):
-
-    def deposit(self, document):
-        """
-        Receive a Xylose document ArticleMeta
-        """
-        pfile = io.BytesIO()
-        doc = pickle.dumps(document)
-        registry_dispatcher_document.delay(doc)
 
     def deposit_by_pids(self, pids_list):
         """
